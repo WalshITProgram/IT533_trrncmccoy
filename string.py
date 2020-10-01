958b4fa84588fdfa9fd16dc0e5d7997178aada2b
trrncmcco <tmccoy2@mail.walshcollege.educlearexit>
2020-10-01 16:47:37 -0400

    commit

---
+++ string.py
@@ -0,0 +1,8 @@
+first_name = "Terrance"
+last_name = "McCoy"
+Password = input("Enter Your Password:\n")
+Passcode = ["!, @, #, $, %, ^, &, *, (, ), -, _, +, +"]
+import random
+Pass_item = random.choice(Passcode)
+
+
