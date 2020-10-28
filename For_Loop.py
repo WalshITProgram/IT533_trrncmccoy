fc2a35fa9ceaef9d9f5437987edd604128768faa
trrncmcco <tmccoy2@mail.walshcollege.educlearexit>
2020-10-27 22:20:05 -0400

    Revise For loop

--- For_Loop.py
+++ For_Loop.py
@@ -3,20 +3,26 @@ programs = True
 ID = []
 Names = []
 Email = []
+Salary = []
+Incorrect_Keys = ['!', '@', '$', '%', '^', '&', '*',' _',  '=', '+', '<', '>',  '?', ';', ':', '[ ]', '{ }']
 while programs:
    Employee_ID = input("Enter Your Employee ID:")
    if len(Employee_ID) <= 7:
        ID.append(Employee_ID)
    Employee_Name = input("Enter Your Full Name:")
-   if Employee_Name.isspace():
+   if Employee_Name:
       Names.append(Employee_Name) 
    Employee_Email = input("Enter Email:")
-   if Employee_Email.isalpha(): 
-      Email.append(Employee_Email)          
+   if Employee_Email.isalnum(): 
+      Email.append(Employee_Email)
+   Employee_Salary = int(input("Enter Salary Between 18 & 27:"))
+   if Employee_Salary >= 18 and Employee_Salary <= 27: 
+      Salary.append(float(Employee_Salary))            
    try: 
       int(Employee_ID)
       str(Employee_Name)
       str(Employee_Email)
+      float(Employee_Salary)
    except:
       print("You entered an invalid ! Please try again.")
 
@@ -28,5 +34,14 @@ while programs:
 print(ID)
 print(Names)
 print(Email)
+print(Salary)
+
+list_of_employees = []
+
+for index, Name in enumerate(Names):
+   employee_dict = {'ID': ID[index], 'Names': Names[index] + " " +  str('IT Department'), 'Email':Email[index], 'Salary': Salary[index], 'Total Salary': Salary[index] * 1.3 }
+   list_of_employees.append(employee_dict)
+   print(list_of_employees)
+
 
     
