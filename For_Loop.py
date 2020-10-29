2f19e6c375e2f6e6cfbf390c88619ac92631c190
trrncmcco <tmccoy2@mail.walshcollege.educlearexit>
2020-10-29 18:52:30 -0400

    commit

--- For_Loop.py
+++ For_Loop.py
@@ -5,32 +5,31 @@ Names = []
 Email = []
 Salary = []
 Incorrect_Keys = ['!', '@', '$', '%', '^', '&', '*',' _',  '=', '+', '<', '>',  '?', ';', ':', '[ ]', '{ }']
-while programs:
-   Employee_ID = input("Enter Your Employee ID:")
-   if len(Employee_ID) <= 7:
-       ID.append(Employee_ID)
-   Employee_Name = input("Enter Your Full Name:")
-   if Employee_Name:
-      Names.append(Employee_Name) 
-   Employee_Email = input("Enter Email:")
-   if Employee_Email.isalnum(): 
-      Email.append(Employee_Email)
-   Employee_Salary = int(input("Enter Salary Between 18 & 27:"))
-   if Employee_Salary >= 18 and Employee_Salary <= 27: 
-      Salary.append(float(Employee_Salary))            
-   try: 
+while programs: 
+      Employee_ID = (input("Enter Your Employee ID:")) 
+      if len(Employee_ID) < 7:
+         ID.append(Employee_ID)
+      Employee_Name = input("Enter Your Full Name:")
+      if str(Employee_Name).isalpha():
+         Names.append(Employee_Name)
+      Employee_Email = input("Enter Email:")
+      if str(Employee_Email).isalnum(): 
+         Email.append(Employee_Email)
+      Employee_Salary = int(input("Enter Salary Between 18 & 27:"))
+      if Employee_Salary >= 18 and Employee_Salary <= 27: 
+         Salary.append(float(Employee_Salary))          
+try:
       int(Employee_ID)
       str(Employee_Name)
       str(Employee_Email)
       float(Employee_Salary)
-   except:
+except:
       print("You entered an invalid ! Please try again.")
-
-   Repeat = input('Would you like to add another record? (Y,N)')
-   if Repeat == 'N':
+Repeat = input('Would you like to add another record? (Y,N)')
+if Repeat == 'N':
       programs = False
       if Repeat =='Y':
-          programs = True
+         programs = True
 print(ID)
 print(Names)
 print(Email)
--- k.py
+++ k.py
@@ -1,91 +1,15 @@
-Employees = [1121, "Jackie Grainger", 22.22, 1122, 
-"Jignesh Thrakkar", 22.25, 1127, "Dion Green", 28.75, 
-False, 24.32, 1132, "Jacob Gerber", "Sarah Sander", 
-23.45, 1137, True, "Brandon Heck", 1138, 25.84, True, 
-1152, "David Toma", 22.65, 23.75, 1157, "David Toma", 
-22.65, 23.75, 1157, "Charles King", False, "Jackie Grainger", 
-1121, 22,22, False, 22.65, 1152, "David Toma"]
-Employees1 = list(filter(bool, Employees))
+programs = True
+Employee_ID = input("Enter Your Employee ID:")
+while programs:
+   try:
+      
+      if len(int(Employee_ID)) <= 7:
+         ID.append(Employee_ID)
+   except:
+      print("You entered an invalid ! Please try again.")
 
-
-Id = [{"Customer ID" : 1121, "Name" : "Jackie Grainger", "Hourly Wage" : 22.22}, 
-{"Customer ID" : 1122, "Name" : "Jignesh Thrakkar", "Hourly Wage" : 22.25}, 
-{"Customer ID" : 1127, "Name" : "Dion Green", "Hourly Wage" : 28.75},
-{"Customer ID" : 1132, "Name" : "Jacob Gerber", "Hourly Wage" : 24.32},
-{"Customer ID" : 1137, "Name" : "Sarah Sander", "Hourly Wage" : 23.45},
-{"Customer ID" : 1138, "Name" : "Brandon Heck", "Hourly Wage": 25.84},
-{"Customer ID" : 1152, "Name" : "David Toma", "Hourly Wage" : 22.65},
-{"Customer ID" : 1157, "Name" : "Charles King", "Hourly Wage" : 23.75}]
-
-#print(sorted(Id, key = lambda i: (i["Hourly Wage"], i["Customer ID"], i["Name"])))
-
-
-    #print(K)
-
-datastore = {"Employees":{
-    "Employee": [
-        {"Employee ID" : 1121,
-         "Name" : "Jackie Grainger", 
-         "Hourly Wage" : 22.22}, 
-        
-        {"Employee ID" : 1122, 
-        "Name" : "Jignesh Thrakkar", 
-        "Hourly Wage" : 25.25}, 
-        
-        {"Employee ID" : 1127, 
-        "Name" : "Dion Green", 
-        "Hourly Wage" : 28.75},
-        
-        {"Employee ID" : 1132, 
-        "Name" : "Jacob Gerber", 
-        "Hourly Wage" : 24.32},
-        
-        {"Employee ID" : 1137, 
-        "Name" : "Sarah Sander", 
-        "Hourly Wage" : 23.45},
-        
-        {"Employee ID" : 1138,
-         "Name" : "Brandon Heck", 
-         "Hourly Wage": 25.84},
-       
-        {"Employee ID" : 1152,
-         "Name" : "David Toma", 
-         "Hourly Wage" : 22.65},
-        
-        {"Employee ID" : 1157,
-         "Name" : "Charles King", 
-         "Hourly Wage" : 23.75}
-
-    ]
-} }
-#print(datastore["Employees"]["Employee"])
-
-underpaid_salaries = []
-company_raises = []
-space = datastore["Employees"]["Employee"]
-for i in space:
-    if i.get('Hourly Wage') == 22.22:
-       i['Hourly Wage'] = 22.22 * 1.05
-       company_raises.append(space[0])
-    if i.get('Hourly Wage') == 25.25:
-       i['Hourly Wage'] = 25.25 * 1.04
-       company_raises.append(space[1])
-    if i.get('Hourly Wage') == 28.75:
-       i['Hourly Wage'] = 28.75 * 1.03
-       company_raises.append([space[2]])
-    if i.get('Hourly Wage') == 24.32:
-       i['Hourly Wage'] = 24.32 * 1.05
-       company_raises.append([space[3]])
-    if i.get('Hourly Wage') == 23.45:
-       i['Hourly Wage'] = 23.45 * 1.05
-       company_raises.append([space[4]])
-    if i.get('Hourly Wage') == 25.84:
-       i['Hourly Wage'] = 25.84 * 1.04
-       company_raises.append(space[5])
-    if i.get('Hourly Wage') == 22.65:
-       i['Hourly Wage'] = 22.65 * 1.05
-       company_raises.append(space[6])
-    if i.get('Hourly Wage') == 23.75:
-       i['Hourly Wage'] = 23.75 * 1.05
-       company_raises.append(space[7])
-    print(f"Employee List: {i}")
+   Repeat = input('Would you like to add another record? (Y,N)')
+   if Repeat == 'N':
+      programs = False
+      if Repeat =='Y':
+          programs = True
