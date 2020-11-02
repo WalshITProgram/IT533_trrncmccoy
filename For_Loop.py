834bea33bd997e155d7aca4448598b536b3856d7
trrncmcco <tmccoy2@mail.walshcollege.educlearexit>
2020-11-02 17:42:41 -0500

    Add comments to code

--- For_Loop.py
+++ For_Loop.py
@@ -1,33 +1,42 @@
 # Create a program that gathers information 
+# List of chars that cannot be included into the employees email 
 Email_Keys = [ '!','\'', "\"", '#', '$', '%', '^', '&', '*', '(', ')',  '=', '+' , '<', '>', '/', '?', ';', ':', '[', ']', '{', '}', '\\' ]
 Address_Keys = ['!', '@', '$', '%', '^', '&', '*', '_', '=', '+', '<', '>', '?', ';', ':', '[', ']', '{', '}']
+# Valid characters for employee name
 valid_name_chars = [" ",'\'','-']
+# The list for dictionary items  
 list_of_employees = []
+# Create a while loop to repeat users input
 while True:
    employee_dict = {}
+   #  employee name
    while True:
       Employee_ID = input("Enter Your Employee ID:")
-      
+      # If less then 7 and not a int must repeat input
       if (len(Employee_ID) <= 7):
+         # If valid add it to employee dict [ID]
          try:
             int(Employee_ID)
             employee_dict['ID'] = Employee_ID
             break
          except: 
             print("You entered an invalid ID number! Please try again.")
-   
+   # Set condition to loop back to the top if user input is empty
    while True:
       entered_name = input("Enter your name:")
       if len(entered_name) == 0:
          continue
       bad_char_hit = False
+      # Create a for loop to iterate through chars entered 
       for letter in entered_name:
+         # Set condition statement to test if the input has letters and valid chars
          if letter not in valid_name_chars and not letter.isalpha():
             bad_char_hit = True
-            
+      # if returned false add employee name      
       if bad_char_hit == False:
          employee_dict['Name'] = entered_name
          break
+      # If emails does not contain emails keys/num/alpha repeats until correct
    while True: 
       Employee_Email = input("Enter Email:")
       if len(Employee_Email) == 0:
@@ -40,6 +49,7 @@ while True:
       if bad_char_hit == False:
          employee_dict['Email'] = Employee_Email
          break
+      # Same sequence of events from line 40-51
    while True:
       Employee_Address = input("Enter Address:")
       bad_char_hit = False
@@ -49,6 +59,7 @@ while True:
       if bad_char_hit == False:
          employee_dict['Address'] = Employee_Address
          break
+      # Convert str to float and check for errors before adding to employee dict
    while True:
       Employee_Salary = input("Enter Salary Between 18 & 27:")
       try:
@@ -61,8 +72,11 @@ while True:
    Repeat = input('Would you like to add another record? (Y,N)')
    if Repeat == 'N':
       break
+   # Create a for loop to iterate through the list of employees 
 for index, employee in enumerate(list_of_employees):
+   # Add IT department 
    list_of_employees[index]["Name"] = list_of_employees[index]["Name"] + "IT Department"
+   # Create key called Total Hourly Salary
    list_of_employees[index]["Total Hourly Salary"] = list_of_employees[index]["Salary"] * 1.3
 for employee in list_of_employees:
    print(employee)
